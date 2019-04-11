dc84202102d74736f2cebe05a46c3744916f6e5b
Michael Wagner <wagner.mchl@googlemail.com>
2019-04-11 20:57:24 +0800

    added: ordering function

--- tuple.go
+++ tuple.go
@@ -22,7 +22,21 @@ const (
 
 	// FLOATPRECISION sets the error for floating point comparison
 	FLOATPRECISION float64 = 0.0000001
+
+	// LT is the `less than` return value for order comparisons
+	LT int = -1
+	// EQ is the `equals` return value for order comparisons
+	EQ int = 0
+	// GT is the `greater than` return value for order comparisons
+	GT int = 1
 )
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
 
 // Elem acts as an element container, holding a generic element and its type indication.
 type Elem struct {
@@ -83,6 +97,90 @@ func (e Elem) isMatching(other *Elem) bool {
 	return false
 }
 
+// Comparator function, used for determining ordering of two elements.
+// The order between elements of different type is arbitrary, but consistent.
+// wildcard < tuple < string < double < int < nil
+// The order between elements of the same type is the builtin in golang
+// Note to self: discussion about value receiver vs pointer receiver:
+// 		https://stackoverflow.com/questions/27775376/value-receiver-vs-pointer-receiver-in-golang
+// Returns 1 if this e > other, -1 if e < other, 0 if both are equal
+func (e Elem) order(other *Elem) int {
+	switch e.elemType {
+
+	case WILDCARD:
+		if other.elemType == WILDCARD {
+			return EQ
+		}
+		return LT
+
+	case TUPLE:
+		switch other.elemType {
+		case WILDCARD:
+			return GT
+		case TUPLE:
+			return e.elemValue.(*Tuple).order(other.elemValue.(*Tuple))
+		default:
+			return LT
+		}
+
+	case STRING:
+		switch other.elemType {
+		case WILDCARD:
+		case TUPLE:
+			return GT
+		case STRING:
+			if e.elemValue.(string) < other.elemValue.(string) {
+				return LT
+			}
+			if e.elemValue.(string) == other.elemValue.(string) {
+				return EQ
+			}
+			return GT
+		case FLOAT:
+		case INT:
+		case NONE:
+			return LT
+		}
+
+	case FLOAT:
+		switch other.elemType {
+		case WILDCARD:
+		case TUPLE:
+		case STRING:
+			return GT
+		case FLOAT:
+			if e.elemValue.(float64) < other.elemValue.(float64) {
+				return LT
+			}
+			if e.elemValue.(float64) == other.elemValue.(float64) {
+				return EQ
+			}
+			return GT
+		case INT:
+		case NONE:
+			return LT
+		}
+
+	case INT:
+		if other.elemType == INT {
+			if e.elemValue.(int32) < other.elemValue.(int32) {
+				return LT
+			}
+			if e.elemValue.(int32) == other.elemValue.(int32) {
+				return EQ
+			}
+			return GT
+		}
+		if other.elemType == NONE {
+			return GT
+		}
+		return LT
+	default:
+		return LT
+	}
+	return LT
+}
+
 // Tuple can contain elements of five different data types:
 // - integers
 // - floating point numbers
@@ -91,6 +189,18 @@ func (e Elem) isMatching(other *Elem) bool {
 // - wildcards
 type Tuple struct {
 	elements []Elem
+}
+
+// MakeTuple creates a new Tuple instance from the given parameters.
+func MakeTuple(element ...Elem) *Tuple {
+	var resultTuple Tuple
+	// For debugging only.
+	for _, e := range element {
+		fmt.Printf("element %v of type %T (reflect %v)\n", e, e, reflect.TypeOf(e))
+	}
+	resultTuple.elements = element
+	fmt.Printf("resulting tuple: %v", resultTuple)
+	return &resultTuple
 }
 
 // IsMatching checks two tuples for equality, which is true if
@@ -115,13 +225,27 @@ func (t *Tuple) IsMatching(other *Tuple) bool {
 	return true
 }
 
-// MakeTuple creates a new Tuple instance from the given parameters.
-func MakeTuple(element ...Elem) *Tuple {
-	var resultTuple Tuple
-	for _, e := range element {
-		fmt.Printf("element %v of type %T (reflect %v)\n", e, e, reflect.TypeOf(e))
+// Comparator function, used for determining ordering of two tuples.
+func (t *Tuple) order(other *Tuple) int {
+	tSize := len(t.elements)
+	otherSize := len(other.elements)
+
+	shorterSize := min(tSize, otherSize)
+
+	// Check each element for equality.
+	for i := 0; i < shorterSize; i++ {
+		if ord := t.elements[i].order(&other.elements[i]); ord != EQ {
+			return ord
+		}
 	}
-	resultTuple.elements = element
-	fmt.Printf("resulting tuple: %v", resultTuple)
-	return &resultTuple
+
+	if tSize < otherSize {
+		return LT
+	}
+
+	if tSize == otherSize {
+		return EQ
+	}
+
+	return GT
 }
