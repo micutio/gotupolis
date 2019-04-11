d6179f2759c14b26116a14954e3b00f9bbccf034
Michael Wagner <wagner.mchl@googlemail.com>
2019-04-11 22:40:47 +0800

    fixed: tests incompatible with changed API

--- tuple_test.go
+++ tuple_test.go
@@ -3,10 +3,10 @@ package gotupolis
 import "testing"
 
 func TestMakeTuple(t *testing.T) {
-	tup1 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(*MakeTuple(S("hurz"))))
+	tup1 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(MakeTuple(S("hurz"))))
 	tup2 := MakeTuple(I(1), A(), F(3.14), S("Furz!"), A())
 	tup3 := MakeTuple(I(1), A(), F(3.140001), S("Furz!"), A())
-	tup4 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(*MakeTuple(A())))
+	tup4 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(MakeTuple(A())))
 
 	if !tup1.IsMatching(tup2) {
 		t.Errorf("Error: tuples %v and %v not matching", tup1, tup2)
@@ -45,9 +45,13 @@ func TestElemOrdering(t *testing.T) {
 
 	tup1 := MakeTuple(I(1), A(), F(3.14), S("Furz!"), A())
 	tup2 := MakeTuple(I(1), A(), F(3.1400), S("Furz!"), A())
-	tup4 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(*MakeTuple(A())))
+	tup4 := MakeTuple(I(1), I(2), F(3.14), S("Furz!"), T(MakeTuple(A())))
 
-	if ord := tup1.order(tup2); ord != LT {
+	if ord := tup1.order(tup2); ord != EQ {
 		t.Errorf("Error: order of tuples %v and %v should be LT, not %v", tup1, tup2, ord)
+	}
+
+	if ord := tup1.order(tup4); ord != LT {
+		t.Errorf("Error: order of tuples %v and %v should be EQ, not %v", tup1, tup4, ord)
 	}
 }
