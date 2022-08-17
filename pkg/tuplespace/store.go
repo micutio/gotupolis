2e87e1f0ab2aa30b31a37afb5885be5e547edefc
micutio <wagner.mchl@googlemail.com>
2022-08-17 17:27:56 +0800

    refactor tuplespace -> store

--- pkg/tuplespace/store.go
+++ pkg/tuplespace/store.go
@@ -1,8 +1,8 @@
 package gotupolis
 
-// The TupleSpace defines an interface that any concrete implementation of a tuple space has to
+// The Store defines an interface that any concrete implementation of a tuple space has to
 // follow.
-type TupleSpace interface {
+type Store interface {
 
 	// Read a tuple that matches the argument and remove it from the space.
 	In(tuple Tuple) Tuple
