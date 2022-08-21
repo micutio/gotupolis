1999ba934953f29716d098f463bdc7f2e8e5864d
micutio <wagner.mchl@googlemail.com>
2022-08-21 22:18:54 +0800

    add SimpleStore implementation

--- pkg/tuplespace/store.go
+++ pkg/tuplespace/store.go
@@ -1,6 +1,8 @@
 package gotupolis
 
 import (
+	"fmt"
+
 	"github.com/tidwall/btree"
 
 	option "github.com/micutio/goptional"
@@ -11,10 +13,10 @@ import (
 type Store interface {
 
 	// Read a tuple that matches the argument and remove it from the space.
-	In(tuple Tuple) option.Maybe[Tuple]
+	In(query Tuple) option.Maybe[Tuple]
 
 	// Read a tuple that matches the argument.
-	Read(tuple Tuple) option.Maybe[Tuple]
+	Read(query Tuple) option.Maybe[Tuple]
 
 	// Write a tuple into the tuple space.
 	Out(tuple Tuple)
@@ -26,4 +28,37 @@ type SimpleStore struct {
 
 func NewSimpleStore() *SimpleStore {
 	return &SimpleStore{tree: btree.NewBTreeG(TupleOrder)}
+}
+
+func (store *SimpleStore) In(query Tuple) option.Maybe[Tuple] {
+	tuple, found := store.tree.Get(query)
+	if found {
+		if tuple.IsMatching(query) {
+			store.tree.Delete(tuple)
+			return option.NewJust(tuple)
+		} else {
+			fmt.Printf("[In] tuple %v does not match query %v\n", tuple, query)
+		}
+	}
+	return option.NewNothing[Tuple]()
+}
+
+func (store *SimpleStore) Read(query Tuple) option.Maybe[Tuple] {
+	tuple, found := store.tree.Get(query)
+	if found {
+		if tuple.IsMatching(query) {
+			return option.NewJust(tuple)
+		} else {
+			fmt.Printf("[Read] tuple %v does not match query %v\n", tuple, query)
+		}
+	}
+	return option.NewNothing[Tuple]()
+}
+
+func (store *SimpleStore) Out(tuple Tuple) {
+	if !tuple.IsDefined() {
+		fmt.Printf("[Out] Warning: attempt to store undefined tuple %v \n", tuple)
+	} else {
+		store.tree.Set(tuple)
+	}
 }
