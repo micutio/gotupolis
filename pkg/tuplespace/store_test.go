90fd48c00b4c68e92faa213f0379f80dd5d6b0ec
micutio <wagner.mchl@googlemail.com>
2022-08-23 01:01:07 +0800

    improve store test cases

--- pkg/tuplespace/store_test.go
+++ pkg/tuplespace/store_test.go
@@ -11,36 +11,46 @@ func TestStore(t *testing.T) {
 	tup := MakeTuple(I(1), I(2), F(3.14), S("Foo!"), T(MakeTuple(S("hurz"))))
 	query := MakeTuple(I(1), Any(), F(3.14), S("Foo!"), T(MakeTuple(S("hurz"))))
 
-	// test insertion
+	// insert for testing defined queries
 	store.Out(tup)
 
-	// test read
 	var tupleOpt1a option.Maybe[Tuple] = store.Read(tup)
 	if !tupleOpt1a.IsPresent() {
-		t.Errorf("Error: cannot find inserted tuple %v", tup)
+		t.Errorf("Error: cannot find tuple %v", tup)
 	}
-	var tupleOpt1b option.Maybe[Tuple] = store.Read(query)
+
+	// since we've only read the tuple, it should still be in the store
+	var tupleOpt1b option.Maybe[Tuple] = store.In(tup)
 	if !tupleOpt1b.IsPresent() {
-		t.Errorf("Error: cannot find inserted tuple %v", query)
-	}
-
-	// test out
-	// // since we've only read the tuple, it should still be in the store
-	// var tupleOpt2a option.Maybe[Tuple] = store.In(tup)
-	// if !tupleOpt2a.IsPresent() {
-	// 	t.Errorf("Error: cannot find inserted tuple %v", tup)
-	// }
-
-	var tupleOpt2b option.Maybe[Tuple] = store.In(query)
-	if !tupleOpt2b.IsPresent() {
-		t.Errorf("Error: cannot find inserted tuple %v", query)
+		t.Errorf("Error: cannot find tuple %v", tup)
 	}
 
 	// now the tuple should be gone
-	var tupleOpt3 option.Maybe[Tuple] = store.In(query)
-	if tupleOpt3.IsPresent() {
+	var tupleOpt1c option.Maybe[Tuple] = store.In(tup)
+	if tupleOpt1c.IsPresent() {
 		t.Errorf("Error: store should not contain tuple %v anymore", tup)
 	}
+
+	// insert for testing with not defined queries
+	store.Out(tup)
+
+	var tupleOpt2a option.Maybe[Tuple] = store.Read(query)
+	if !tupleOpt2a.IsPresent() {
+		t.Errorf("Error: cannot find tuple %v", query)
+	}
+
+	// since we've only read the tuple, it should still be in the store
+	var tupleOpt2b option.Maybe[Tuple] = store.In(query)
+	if !tupleOpt2b.IsPresent() {
+		t.Errorf("Error: cannot find tuple %v", query)
+	}
+
+	// now the tuple should be gone
+	var tupleOpt2c option.Maybe[Tuple] = store.In(query)
+	if tupleOpt2c.IsPresent() {
+		t.Errorf("Error: store should not contain tuple %v anymore", tup)
+	}
+
 }
 
 func getStoreImpl() Store {
